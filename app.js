af3af7186f19e4d2438a54ecba0fd99ea5371c61
Roman Espino <espinosanchez07@gmail.com>
2024-03-18 15:10:00 -0600

    fix: se agrego el cuerpo en el html y se creo el archivo app.js con la estructura try catch y la url de la poke api

---
+++ app.js
@@ -0,0 +1,15 @@
+const d = document,
+    $main = d.querySelector("main"),
+    $links = d.querySelector(".links");
+
+let urlAPI = "https://pokeapi.co/api/v2/pokemon/";
+
+async function loadPokemons(url) {
+    try {
+        $
+    } catch (err) {
+        
+    }
+}
+
+d.addEventListener("DOMContentLoaded", e => loadPokemons(urlAPI));

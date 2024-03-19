5e30ddf5cfdf637c1adc468da4e9f0a1b253a331
Roman Espino <espinosanchez07@gmail.com>
2024-03-19 00:39:48 -0600

    feat: se agrego modal

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ async function loadPokemons(url) {
                 if(!res.ok) throw { status: res.status, statusText: res.statusText }
 
                 $template += `
-                    <figure class="pokemon-card">
+                    <figure class="pokemon-card" data-pokemon="${json.results[i].url}">
                         <img class="pokemon-img" src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
                         <figcaption>${pokemon.name} -- type: ${pokemon.types[0].type.name}</figcaption>
                     </figure>
@@ -58,13 +58,51 @@ async function loadPokemons(url) {
 
 d.addEventListener("DOMContentLoaded", e => loadPokemons(urlAPI));
 
-d.addEventListener("click", e => {
+
+d.addEventListener("click", async e => {
     if(e.target.matches(".links a")){
         e.preventDefault();
         loadPokemons(e.target.getAttribute("href"));
     }
 
     if (e.target.matches(".pokemon-card, .pokemon-card *")) {
-        alert("HOla");
+        let $card = e.target.closest(".pokemon-card");
+        if ($card) {
+            //console.log($card.dataset.pokemon);
+            let apiURL = $card.dataset.pokemon;
+            let res = await fetch(apiURL);
+                
+            if (!res.ok) {
+                throw new Error(`HTTP error! status: ${res.status}`);
+            } else {
+                let pokemon = await res.json(),
+                    $modal = d.getElementById("modal"),
+                    abilities = pokemon.abilities.map(ability => `<li>${ability.ability.name}</li>`).join(''),
+                    $template = `
+                        <div class="modal-content">
+                            <h2>${pokemon.name}</h2>
+                            <img class="pokemon-img-modal" src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
+                            <p>Type: ${pokemon.types[0].type.name}</p>
+                            <p>Abilities: 
+                                <ul>
+                                    ${abilities}
+                                </ul>
+                            </p>
+                            <!-- Aquí puedes agregar más características del Pokémon -->
+                        </div>
+                    `;
+                $modal.innerHTML = $template;
+                $modal.style.display = "block"; 
+            }
+        }
     }
 });
+
+// Cierra el modal cuando haces clic fuera
+window.onclick = function(event) {
+    let $modal = d.querySelector("#modal");
+    if (event.target == $modal) {
+        $modal.style.display = "none";
+    }
+}
+
